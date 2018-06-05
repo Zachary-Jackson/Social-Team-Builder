e683740f2a45f841fa76d70cce48fd5f0866516e
Zachary-Jackson <jackson.zachary.r@gmail.com>
2018-06-05 00:29:22 -0400

    Increased profiles' test coverage
    The test suite for the profiles app has been increased significantly.

--- team_builder/profiles/tests.py
+++ team_builder/profiles/tests.py
@@ -10,10 +10,17 @@ class ProfileViewsTests(TestCase):
     def setUp(self):
         """Creates a Profile and some skills for testing"""
         # Get the current User model
-        User = get_user_model()
-        self.user = User.objects.create_user(
+        # A Profile object will be added to this user
+        user = get_user_model()
+        self.user = user.objects.create_user(
             email='test@test.com',
-            password='verysecret1'
+            password='testpass'
+        )
+
+        # Create a new user without a Profile object
+        self.user_no_profile = user.objects.create_user(
+            email='no@profile.com',
+            password='testpass'
         )
 
         # Creates a couple of Skill(s)
@@ -25,9 +32,9 @@ class ProfileViewsTests(TestCase):
         self.profile = Profile.objects.create(
             user=self.user,
             username='Santa',
-            bio='The weather forcast shows snow for the next five months.',
+            bio='The weather forecast shows snow for the next five months.',
         )
-        self.profile.skills.add(self.skill_1)
+        self.profile.skills.add(self.skill_1, self.skill_2)
 
         # Creates a Position to attach to a Project
         self.position = Position.objects.create(
@@ -38,16 +45,94 @@ class ProfileViewsTests(TestCase):
         self.project = Project.objects.create(
             owner=self.profile, title='Team Builder',
             time_line='Depends on the number of features',
-            requirements='See the README.md', description='also see README.MD'
+            requirements='See the README.md', description='also see README.md'
         )
         self.project.positions.add(self.position)
 
+    def test_homepage(self):
+        """Tests the homepage's content"""
+        resp = self.client.get(reverse('profiles:homepage'))
+
+        # projects that need field and their needs
+        self.assertContains(resp, 'Team Builder')
+        self.assertContains(resp, 'Django')
+        # various page information
+        self.assertContains(resp, 'Projects')
+        self.assertContains(resp, 'All Needs')
+        self.assertContains(resp, 'Projects')
+
+    def test_login_router_with_profile(self):
+        """Tests the router if the user has a profile"""
+        self.client.login(username='test@test.com', password='testpass')
+        resp = self.client.get(reverse('profiles:login_router'))
+        self.assertRedirects(
+            resp, reverse('profiles:profile', kwargs={'pk': self.profile.pk}))
+
+    def test_login_router_without_profile(self):
+        """Tests the router if the user does not have a profile"""
+        self.client.login(username='no@profile.com', password='testpass')
+        resp = self.client.get(reverse('profiles:login_router'))
+
+        # Checks that a second Profile was created
+        self.assertEqual(2, len(Profile.objects.all()))
+
+        self.assertRedirects(resp, reverse('profiles:edit'))
+
+    """Profile tests"""
+
     def test_profile_edit_not_logged_in(self):
         """Ensures that profile_edit requires log in"""
+        # @login_required does this, but in case its deleted somehow
         resp = self.client.get(reverse('profiles:edit'))
 
         # The user has not logged in, so we should get redirected
         self.assertEqual(resp.status_code, 302)
+
+    def test_profile_edit(self):
+        """Ensures that profile_edit appears correctly"""
+        self.client.login(username='test@test.com', password='testpass')
+        resp = self.client.get(reverse('profiles:edit'))
+
+        # our profile information
+        self.assertContains(resp, 'Santa')
+        self.assertContains(resp, 'The weather forecast shows snow for the')
+        # various page information
+        self.assertContains(resp, 'Applications')
+        self.assertContains(resp, 'Past Projects')
+        self.assertContains(resp, 'My Skills')
+        self.assertContains(resp, 'Save Changes')
+        self.assertContains(resp, 'Profile')
+
+    def test_profile_edit_post(self):
+        """Ensures that profile_edit can update the Profile"""
+        self.client.login(username='test@test.com', password='testpass')
+        self.client.post(
+            reverse('profiles:edit'),
+            data={'bio': 'new bio', 'username': 'editor'})
+
+        # get the updated profile for testing
+        profile = Profile.objects.get(pk = self.profile.pk)
+
+        self.assertEqual(profile.bio, 'new bio')
+        self.assertEqual(profile.username, 'editor')
+
+    def test_profile_view(self):
+        """Ensures the profile_view is working"""
+        self.client.login(username='test@test.com', password='testpass')
+        resp = self.client.get(
+            reverse('profiles:profile', kwargs={'pk': self.profile.pk}))
+
+        self.assertContains(resp, "Santa's profile")
+        self.assertContains(resp, 'The weather forecast shows snow for')
+        self.assertContains(resp, 'Past Projects')
+        self.assertContains(resp, 'Edit')
+        self.assertContains(resp, 'Django')
+        self.assertContains(resp, 'Angular')
+        # The edit button should appear to allow the logged in user to edit
+        # their profile
+        self.assertContains(resp, 'Edit')
+
+    """Project tests"""
 
     def test_project_view_not_logged_in(self):
         """Ensures that the project view page is working"""
@@ -60,7 +145,7 @@ class ProfileViewsTests(TestCase):
         self.assertContains(resp, 'Team Builder')
         self.assertContains(resp, 'Depends on the number of features')
         self.assertContains(resp, 'See the README.md')
-        self.assertContains(resp, 'also see README.MD')
+        self.assertContains(resp, 'also see README.md')
         # This is the position parts of the project
         self.assertContains(resp, 'this is the position')
         self.assertContains(resp, 'Django')
@@ -69,15 +154,67 @@ class ProfileViewsTests(TestCase):
         self.assertNotContains(resp, 'Edit Project')
         self.assertNotContains(resp, 'Delete Project')
 
+    def test_project_view(self):
+        """Logged in test"""
+        self.client.login(username='test@test.com', password='testpass')
+        resp = self.client.get(
+            reverse('profiles:project', kwargs={'pk': self.project.pk}))
+
+        # Because we are logged in, the following should appear
+        self.assertContains(resp, 'Edit Project')
+        self.assertContains(resp, 'Delete Project')
+
     def test_project_view_bad_pk(self):
         """We should get a 404 error"""
         resp = self.client.get(
             reverse('profiles:project', kwargs={'pk': 10947}))
         self.assertEqual(resp.status_code, 404)
 
-
     def test_project_edit_not_logged_in(self):
         """We should be routed to the login page"""
         resp = self.client.get(
             reverse('profiles:project_edit', kwargs={'pk': self.project.pk}))
         self.assertEqual(resp.status_code, 302)
+
+    def test_project_edit(self):
+        """Ensures project_edit appears correctly"""
+        self.client.login(username='test@test.com', password='testpass')
+        resp = self.client.get(
+            reverse('profiles:project_edit', kwargs={'pk': self.project.pk}))
+
+        # project information
+        self.assertContains(resp, 'Team Builder')
+        self.assertContains(resp, 'Depends on the number of features')
+        self.assertContains(resp, 'See the README.md')
+        self.assertContains(resp, 'also see README.md')
+        # page information
+        self.assertContains(resp, 'Project Timeline')
+        self.assertContains(resp, 'Save Changes')
+        self.assertContains(resp, 'Delete Project')
+
+    def test_project_edit_post(self):
+        """Ensures project_edit appears correctly"""
+        self.client.login(username='test@test.com', password='testpass')
+        resp = self.client.post(
+            reverse('profiles:project_edit', kwargs={'pk': self.project.pk}),
+            data={
+                'title': 'test post', 'time_line': 'milliseconds',
+                'requirements': 'data to post', 'description': 'description'
+            }
+        )
+
+        # Get the updated Project
+        project = Project.objects.get(pk=self.project.pk)
+        self.assertEqual(project.title, 'test post')
+        self.assertEqual(project.time_line, 'milliseconds')
+        self.assertEqual(project.requirements, 'data to post')
+        self.assertEqual(project.description, 'description')
+
+    def test_project_edit_unowned(self):
+        """Tests to make sure a loged in user can not edit other's Projects"""
+        self.client.login(username='no@profile.com', password='testpass')
+        resp = self.client.get(
+            reverse('profiles:project_edit', kwargs={'pk': self.project.pk}))
+
+        # Ensure we were kicked out
+        self.assertEqual(resp.status_code, 404)
